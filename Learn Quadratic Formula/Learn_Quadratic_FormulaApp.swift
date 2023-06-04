69e3e742ea11c89817bb4c86b3fad4815fe46d2f
BGSMM <yoonbumtae@gmail.com>
2023-06-04 18:32:19 +0900

    Initial Commit

---
+++ Learn Quadratic Formula/Learn_Quadratic_FormulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_Quadratic_FormulaApp.swift
+//  Learn Quadratic Formula
+//
+//  Created by 윤범태 on 2023/06/04.
+//
+
+import SwiftUI
+
+@main
+struct Learn_Quadratic_FormulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
